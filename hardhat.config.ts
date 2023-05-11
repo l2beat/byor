7b958b02922b4c6c5adc8fb652baf3adac9b0cec
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-05-11 12:39:14 +0200

    Formatting and linting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,13 +2,13 @@ import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 
 const config: HardhatUserConfig = {
-    solidity: '0.8.18',
-    paths: {
-        sources: "./packages/rollup/contracts",
-        tests: "./packages/rollup/test",
-        cache: "./build/hardhat/cache",
-        artifacts: "./build/hardhat/artifacts"
-    },
+  solidity: '0.8.18',
+  paths: {
+    sources: './packages/rollup/contracts',
+    tests: './packages/rollup/test',
+    cache: './build/hardhat/cache',
+    artifacts: './build/hardhat/artifacts',
+  },
 }
 
 export default config
--- packages/rollup/test/CTCTest.ts
+++ packages/rollup/test/CTCTest.ts
@@ -1,26 +1,26 @@
-import { assert, expect } from 'chai'
+import { expect } from 'chai'
 import hre from 'hardhat'
 
-describe('CTC', function() {
-    const randomBytes = '0x12345678907654321234567890987654321234567890987654'
+describe('CTC', function () {
+  const randomBytes = '0x12345678907654321234567890987654321234567890987654'
 
-    it('Should emit BatchAppended event', async function() {
-        const [deployer] = await hre.ethers.getSigners()
+  it('Should emit BatchAppended event', async function () {
+    const [deployer] = await hre.ethers.getSigners()
 
-        const CTC = await hre.ethers.getContractFactory('CTC')
-        const ctc = await CTC.deploy()
+    const CTC = await hre.ethers.getContractFactory('CTC')
+    const ctc = await CTC.deploy()
 
-        await expect(ctc.appendBatch(randomBytes))
-            .to.emit(ctc, "BatchAppended")
-            .withArgs(await deployer?.getAddress())
-    })
+    await expect(ctc.appendBatch(randomBytes))
+      .to.emit(ctc, 'BatchAppended')
+      .withArgs(await deployer?.getAddress())
+  })
 
-    it('Should revert if called from another contract', async function() {
-        const CTC = await hre.ethers.getContractFactory('CTC')
-        const ctc = await CTC.deploy()
-        const CTCRevert = await hre.ethers.getContractFactory('CTCRevert')
-        const ctcRevert = await CTCRevert.deploy(ctc.address)
+  it('Should revert if called from another contract', async function () {
+    const CTC = await hre.ethers.getContractFactory('CTC')
+    const ctc = await CTC.deploy()
+    const CTCRevert = await hre.ethers.getContractFactory('CTCRevert')
+    const ctcRevert = await CTCRevert.deploy(ctc.address)
 
-        await expect(ctcRevert.appendBatch(randomBytes)).to.be.reverted
-    })
+    await expect(ctcRevert.appendBatch(randomBytes)).to.be.reverted
+  })
 })
