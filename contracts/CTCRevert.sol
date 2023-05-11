238a9a763c1baece72ebad4a5b38c4e1a74386ce
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-05-11 10:33:47 +0200

    Add CTC contract

---
+++ contracts/CTCRevert.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./CTC.sol";
+
+contract CTCRevert {
+    CTC ctc;
+    constructor(address ctcContract) {
+        ctc = CTC(ctcContract);
+    }
+
+    function appendBatch(bytes calldata batch) public {
+        ctc.appendBatch(batch);
+    }
+}
