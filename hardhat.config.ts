6242f24ff1aa376a89e3543d8c5119dda373a949
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-05-11 12:34:33 +0200

    Refactor paths and tests to use hardhat chai extensions

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,13 @@ import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.18',
+    solidity: '0.8.18',
+    paths: {
+        sources: "./packages/rollup/contracts",
+        tests: "./packages/rollup/test",
+        cache: "./build/hardhat/cache",
+        artifacts: "./build/hardhat/artifacts"
+    },
 }
 
 export default config
