bb862f62b7391ea5359d70ec81b569d7e0d39bd9
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-05-15 09:20:20 +0200

    Update tests

--- packages/szk/src/Serialize.test.ts
+++ packages/szk/src/Serialize.test.ts
@@ -9,6 +9,7 @@ import {
   Transaction,
 } from './types/Transactions'
 import { Unsigned64 } from './types/UnsignedSized'
+import { Hex } from 'viem'
 
 const modelAccount = privateKeyToAccount(
   '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
@@ -23,13 +24,14 @@ const modelTx: Transaction = {
   hash: '0x343d48c0e2c7852c9483a53a4017b7ab586140f0a0e31bc1b9e2e20a9900ea48',
 }
 
+const modelTxSerializedHex =
+  '0x70997970C51812dc3A010C7d01b50e0d17dc79C8000000000000000a00000000000000010000000000000002950e2f5c8514196afc5ba38e0d10638d3f4061d6d0b62573ad47808587f92f9867d72774c53d2e64d4fcc6fb9f5526be2a93a68514109d0292c13656f481d0331b'
+
 describe('serialize', function () {
   it('Should serialize a valid transaction', async function () {
     const serialized = await serialize(modelTx, modelAccount)
     expect(serialized.length).to.equal(SIGNED_TX_HEX_SIZE * 2 + 2)
-    expect(serialized).to.equal(
-      '0x70997970C51812dc3A010C7d01b50e0d17dc79C8000000000000000a00000000000000010000000000000002950e2f5c8514196afc5ba38e0d10638d3f4061d6d0b62573ad47808587f92f9867d72774c53d2e64d4fcc6fb9f5526be2a93a68514109d0292c13656f481d0331b',
-    )
+    expect(serialized).to.equal(modelTxSerializedHex)
   })
 })
 
@@ -41,10 +43,8 @@ describe('deserialize', function () {
     expect(E.isRight(deserialized)).true
   })
 
-  it('Should error on invalid signature', async function () {
-    const signedTxBytes =
-      '0x70997970C51812dc3A010C7d01b50e0d17dc79C8000000000000000a00000000000000010000000000000002950e2f5c8514196afc5ba38e0d10638d3f4061d6d0b62573ad47808587f92f9867d72774c53d2e64d4fcc6fb9f5526be2a93a68514109d0292c13123f481d0331b'
-
+  it('Should deserialize but not be equal to the model transaction after message corruption', async function () {
+    const signedTxBytes: Hex = `0xdead${modelTxSerializedHex.slice(6)}`
     const deserialized = await deserialize(signedTxBytes)
     expect(E.isLeft(deserialized)).true
 
@@ -53,8 +53,7 @@ describe('deserialize', function () {
   })
 
   it('Should deserialize a valid input', async function () {
-    const signedTxBytes =
-      '0x70997970C51812dc3A010C7d01b50e0d17dc79C8000000000000000a00000000000000010000000000000002950e2f5c8514196afc5ba38e0d10638d3f4061d6d0b62573ad47808587f92f9867d72774c53d2e64d4fcc6fb9f5526be2a93a68514109d0292c13656f481d0331b'
+    const signedTxBytes = modelTxSerializedHex
 
     const deserialized = await deserialize(signedTxBytes)
     expect(E.isLeft(deserialized)).true
