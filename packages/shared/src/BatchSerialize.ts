54d7c764e3d42d112d406b901421626d6c02b1ac
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-05-16 12:25:59 +0200

    Introduce SIGNED_TX_ASCII_SIZE

--- packages/shared/src/BatchSerialize.ts
+++ packages/shared/src/BatchSerialize.ts
@@ -3,7 +3,7 @@ import { PrivateKeyAccount } from 'viem'
 import { deserialize, serializeAndSign } from './Serialize'
 import { TransactionBatch } from './types/Batch'
 import { Hex } from './types/Hex'
-import { SIGNED_TX_SIZE, Transaction } from './types/Transactions'
+import { SIGNED_TX_ASCII_SIZE, Transaction } from './types/Transactions'
 
 export async function serializeAndSignBatch(
   unsignedBatch: TransactionBatch,
@@ -24,17 +24,17 @@ export async function deserializeBatch(
   const result = new Array<Transaction>()
 
   const bytes = signedBatchBytes.slice(2)
-  if (bytes.length % (SIGNED_TX_SIZE * 2) !== 0) {
+  if (bytes.length % SIGNED_TX_ASCII_SIZE !== 0) {
     throw new Error(
       'Length of input bytes is not multiple of SIGNED_TX_HEX_SIZE',
     )
   }
 
-  const txCount = bytes.length / (SIGNED_TX_SIZE * 2)
+  const txCount = bytes.length / SIGNED_TX_ASCII_SIZE
 
   for (let i = 0; i < txCount; i++) {
     const signedTxBytes = Hex(
-      bytes.slice(i * (SIGNED_TX_SIZE * 2), (i + 1) * (SIGNED_TX_SIZE * 2)),
+      bytes.slice(i * SIGNED_TX_ASCII_SIZE, (i + 1) * SIGNED_TX_ASCII_SIZE),
     )
 
     result.push(await deserialize(signedTxBytes))
