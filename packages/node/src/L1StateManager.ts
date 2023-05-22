811410b431dd13f5800e8cacc2f7b5dc64bd1e52
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-05-22 13:35:11 +0200

    Move executeBatch to TransactionExecutor

--- packages/node/src/L1StateManager.ts
+++ packages/node/src/L1StateManager.ts
@@ -3,7 +3,6 @@ import {
   deserializeBatch,
   EthereumAddress,
   Hex,
-  TransactionBatch,
   Unsigned64,
 } from '@byor/shared'
 import {
@@ -20,6 +19,7 @@ import { Chain, mainnet } from 'viem/chains'
 import { Config } from './config'
 import { AccountRepository } from './db/AccountRepository'
 import { Database } from './db/Database'
+import { StateMap, TransactionExecutor } from './TransactionExecutor'
 
 const abi = parseAbiItem('event BatchAppended(address sender)')
 type EventAbiType = typeof abi
@@ -104,90 +104,17 @@ export class L1StateManager {
       l1State.map((hex) => deserializeBatch(hex)),
     )
 
-    interface StateMapValue {
-      balance: Unsigned64
-      nonce: Unsigned64
-    }
-    type StateMap = Record<string, StateMapValue>
-
-    const executeBatch = (
-      state: StateMap,
-      batch: TransactionBatch,
-      batchPoster: EthereumAddress,
-    ): void => {
-      const getOrInsert = (
-        state: StateMap,
-        key: string,
-        defaultValue: StateMapValue,
-      ): StateMapValue => {
-        const result = state[key]
-        if (result !== undefined) {
-          return result
-        } else {
-          state[key] = { ...defaultValue }
-          // NOTE(radomski): We know that it won't be undefined we insert into that slot in the above line
-          // eslint-disable-next-line
-          return state[key]!
-        }
-      }
-
-      const defaultState: StateMapValue = {
-        balance: Unsigned64(0),
-        nonce: Unsigned64(0),
-      }
-
-      const feeRecipientAccount = getOrInsert(
-        state,
-        batchPoster.toString(),
-        defaultState,
-      )
-      for (const tx of batch) {
-        // Step 0. Check transaction type
-        assert(tx.from !== Hex(0) && tx.to !== Hex(0))
-
-        const fromAccount = getOrInsert(state, tx.from.toString(), defaultState)
-        const toAccount = getOrInsert(state, tx.to.toString(), defaultState)
-
-        // Step 1. Update nonce
-        assert(
-          fromAccount.nonce === Unsigned64(Unsigned64.toBigInt(tx.nonce) - 1n),
-        )
-        fromAccount.nonce = tx.nonce
-
-        // Step 2. Subtract spending
-        assert(
-          Unsigned64.toBigInt(fromAccount.balance) >=
-            Unsigned64.toBigInt(tx.value) + Unsigned64.toBigInt(tx.fee),
-        )
-
-        fromAccount.balance = Unsigned64(
-          Unsigned64.toBigInt(fromAccount.balance) -
-            (Unsigned64.toBigInt(tx.value) + Unsigned64.toBigInt(tx.fee)),
-        )
-
-        // Step 3. Transfer value
-        toAccount.balance = Unsigned64(
-          Unsigned64.toBigInt(tx.value) +
-            Unsigned64.toBigInt(toAccount.balance),
-        )
-
-        // Step 4. Pay fee
-        feeRecipientAccount.balance = Unsigned64(
-          Unsigned64.toBigInt(tx.fee) +
-            Unsigned64.toBigInt(feeRecipientAccount.balance),
-        )
-      }
-    }
-
-    const state: StateMap = {}
+    const initialState: StateMap = {}
     const accountRepository = new AccountRepository(database)
     accountRepository.getAll().forEach(
       (acc) =>
-        (state[acc.address] = {
+        (initialState[acc.address] = {
           balance: Unsigned64(acc.balance),
           nonce: Unsigned64(acc.nonce),
         }),
     )
+
+    const txExecutor = new TransactionExecutor(initialState)
 
     assert(
       batches.length === callDataPosters.length,
@@ -196,11 +123,12 @@ export class L1StateManager {
     for (let i = 0; i < batches.length; i++) {
       // NOTE(radomski): We know that it won't be undefined because of the assert above this for loop
       // eslint-disable-next-line
-      executeBatch(state, batches[i]!, callDataPosters[i]!)
+      txExecutor.executeBatch(batches[i]!, callDataPosters[i]!)
     }
 
+    const updatedState = txExecutor.finalize()
     accountRepository.addOrUpdateMany(
-      Object.entries(state).map(([address, value]) => {
+      Object.entries(updatedState).map(([address, value]) => {
         // WARNING(radomski): This can fail very badly if the value represented
         // by 'BigInt' is so big that the floating point nature of 'number'
         // causes it to lose precision. This can happen when the value is
