230926f6ed5b366eb0de8a6bc9ac408d82bd7551
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-05-23 15:46:52 +0200

    TransactionExecutor is now a function

--- packages/node/src/L1StateManager.ts
+++ packages/node/src/L1StateManager.ts
@@ -2,9 +2,9 @@ import { deserializeBatch, EthereumAddress } from '@byor/shared'
 import { zip } from 'lodash'
 
 import { AccountRepository } from './db/AccountRepository'
+import { executeBatch, StateMap } from './executeBatch'
 import { L1EventStateType } from './L1EventStateType'
 import { L1StateFetcher } from './L1StateFetcher'
-import { StateMap, TransactionExecutor } from './TransactionExecutor'
 
 export class L1StateManager {
   private readonly accountRepository: AccountRepository
@@ -25,27 +25,24 @@ export class L1StateManager {
       l1States.map((state) => deserializeBatch(state.calldata)),
     )
 
-    const initialState: StateMap = {}
+    let accountState: StateMap = {}
     this.accountRepository.getAll().forEach(
       (acc) =>
-        (initialState[acc.address.toString()] = {
+        (accountState[acc.address.toString()] = {
           balance: acc.balance,
           nonce: acc.nonce,
         }),
     )
 
-    const txExecutor = new TransactionExecutor(initialState)
-
     for (const [batch, state] of zip(batches, l1States)) {
       // NOTE(radomski): We know that it won't be undefined
       // because of the assert at the beginning of this function
       // eslint-disable-next-line
-      txExecutor.executeBatch(batch!, state!.poster)
+      accountState = executeBatch(accountState, batch!, state!.poster)
     }
 
-    const updatedState = txExecutor.finalize()
     this.accountRepository.addOrUpdateMany(
-      Object.entries(updatedState).map(([address, value]) => {
+      Object.entries(accountState).map(([address, value]) => {
         return {
           address: EthereumAddress(address),
           balance: value.balance,
