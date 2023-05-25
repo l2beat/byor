b29cd8dd5ed425be4baf47c7526f0d64fa7cbff6
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-05-25 08:11:40 +0200

    Happy path for router test

--- packages/node/src/api/routers/AccountRouter.test.ts
+++ packages/node/src/api/routers/AccountRouter.test.ts
@@ -6,6 +6,28 @@ import { createAccountRouter } from './AccountRouter'
 import { createTestApiServer } from './test/createTestApiServer'
 
 describe(createAccountRouter.name, () => {
+  it('returns an existing account', async () => {
+    const accountRepository = mockObject<AccountRepository>({
+      getByAddressOrDefault: mockFn().returnsOnce({
+        address: EthereumAddress('0xaabbccddeeffaabbccddeeffaabbccddeeffaabb'),
+        balance: Unsigned64(1410),
+        nonce: Unsigned64(59),
+      }),
+    })
+    const router = createAccountRouter(accountRepository)
+    const server = createTestApiServer({ router })
+
+    await server
+      .get(
+        '/router.getState?input="0xaabbccddeeffaabbccddeeffaabbccddeeffaabb"',
+      )
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        '{"result":{"data":{"address":"0xaabbccddeeffaabbccddeeffaabbccddeeffaabb","balance":"1410","nonce":"59"}}}',
+      )
+  })
+
   it('returns an account on non-existent valid address', async () => {
     const accountRepository = mockObject<AccountRepository>({
       getByAddressOrDefault: mockFn().returnsOnce({
@@ -21,8 +43,11 @@ describe(createAccountRouter.name, () => {
       .get(
         '/router.getState?input="0x0000000000000000000000000000000000000000"',
       )
-      .expect(200)
-    await server.get('/router.getState').expect(400)
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        '{"result":{"data":{"address":"0x0000000000000000000000000000000000000000","balance":"0","nonce":"0"}}}',
+      )
   })
 
   it('returns bad request on no data', async () => {
@@ -36,7 +61,10 @@ describe(createAccountRouter.name, () => {
     const router = createAccountRouter(accountRepository)
     const server = createTestApiServer({ router })
 
-    await server.get('/router.getState').expect(400)
+    await server
+      .get('/router.getState')
+      .expect('Content-Type', /json/)
+      .expect(400)
   })
 
   it('returns bad request on invalid data', async () => {
@@ -52,12 +80,17 @@ describe(createAccountRouter.name, () => {
 
     await server
       .get('/router.getState?input="0xthisistooshortandnothex"')
+      .expect('Content-Type', /json/)
       .expect(400)
-    await server.get('/router.getState?input="0x1234"').expect(400)
+    await server
+      .get('/router.getState?input="0x1234"')
+      .expect('Content-Type', /json/)
+      .expect(400)
     await server
       .get(
         '/router.getState?input="0x00000000000000000000000000000000000000000"',
       )
+      .expect('Content-Type', /json/)
       .expect(400)
   })
 })
