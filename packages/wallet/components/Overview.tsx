b4c0897e18968ee6b1b0f47a18ceaa1e68619ff3
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-05-31 15:19:44 +0200

    Overview and the same place as before

---
+++ packages/wallet/components/Overview.tsx
@@ -0,0 +1,31 @@
+import { Separator } from '@/components/ui/separator'
+
+function Statistic({ description, value }): JSX.Element {
+  return (
+    <div className="font-sens-serif uppercase py-1">
+      <div>{`${description}`}</div>
+      <h1 className="text-2xl">{`${value}`}</h1>
+    </div>
+  )
+}
+
+export function Overview(): JSX.Element {
+  return (
+    <div className="container flex border rounded p-6 justify-around">
+      <div>
+        <Statistic description={'L2 Transactions'} value={`test`} />
+        <Separator />
+        <Statistic
+          description={'L1 Last Batch Upload'}
+          value={'13:43:15 18.05.2023'}
+        />
+      </div>
+      <Separator orientation="vertical" />
+      <div>
+        <Statistic description={'Daily Transaction Count'} value={'175'} />
+        <Separator />
+        <Statistic description={'Daily Token Volume'} value={'23403'} />
+      </div>
+    </div>
+  )
+}
--- packages/wallet/pages/index.tsx
+++ packages/wallet/pages/index.tsx
@@ -1,9 +1,12 @@
+import { Overview } from '@/components/Overview'
+
 import { Navbar } from '../components/Navbar'
 
 export default function Home() {
   return (
     <main>
-        <Navbar />
+      <Navbar />
+      <Overview />
     </main>
   )
 }
