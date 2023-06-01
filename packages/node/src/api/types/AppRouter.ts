60953713cd7c3bd99f9919b4ddf62944cf2c78af
Krzysztof Kaczor <krzkaczor@gmail.com>
2023-06-01 10:05:24 +0200

    fix types

--- packages/node/src/api/types/AppRouter.ts
+++ packages/node/src/api/types/AppRouter.ts
@@ -1,13 +1,14 @@
-import { createAccountRouter } from "../routers/AccountRouter"
-import { createTransactionRouter } from "../routers/TransactionRouter"
+import { createAccountRouter } from '../routers/AccountRouter'
+import { createTransactionRouter } from '../routers/TransactionRouter'
 import { router } from '../trpc'
+
+export function makeRouter(routes: AppRouter) {
+  return router(routes)
+}
 
 export type AppRouter = {
   accounts: ReturnType<typeof createAccountRouter>
   transactions: ReturnType<typeof createTransactionRouter>
 }
 
-// export type AppRouter = ReturnType< typeof router({
-//   accounts: createAccountRouter(undefined!),
-//   transactions: createTransactionRouter(undefined!)
-// })>
+export type RootRouter = ReturnType<typeof makeRouter>
