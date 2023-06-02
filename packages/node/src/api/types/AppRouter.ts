5037a3c0d5df56d103399bf4ecccfab5f4935ba1
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-06-02 19:02:14 +0200

    Remove type error from tRPC

--- packages/node/src/api/types/AppRouter.ts
+++ packages/node/src/api/types/AppRouter.ts
@@ -1,16 +1,17 @@
+import { ProcedureRouterRecord } from '@trpc/server'
 import { createAccountRouter } from '../routers/AccountRouter'
-import { createTransactionRouter } from '../routers/TransactionRouter'
 import { createStatisticsRouter } from '../routers/StatisticRouter'
+import { createTransactionRouter } from '../routers/TransactionRouter'
 import { router } from '../trpc'
 
-export function makeRouter(routes: AppRouter) {
-  return router(routes)
-}
-
-export type AppRouter = {
+export interface AppRouter extends ProcedureRouterRecord {
   accounts: ReturnType<typeof createAccountRouter>
   transactions: ReturnType<typeof createTransactionRouter>
   statistics: ReturnType<typeof createStatisticsRouter>
+}
+
+export function makeRouter(routes: AppRouter) {
+  return router(routes)
 }
 
 export type RootRouter = ReturnType<typeof makeRouter>
