bb58958d7beb84663827f8475696acb2e6284ef9
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-06-05 13:03:20 +0200

    remove getLogsSinceGenesis, add more tests

--- packages/node/src/peripherals/ethereum/EthereumClient.test.ts
+++ packages/node/src/peripherals/ethereum/EthereumClient.test.ts
@@ -1,11 +1,11 @@
-import { Hex, Logger } from '@byor/shared'
+import { EthereumAddress, Hex, Logger } from '@byor/shared'
 import { expect, mockFn, mockObject } from 'earl'
-import { parseAbiItem, PublicClient } from 'viem'
+import { Hex as ViemHex, parseAbiItem, PublicClient } from 'viem'
 
 import { EthereumClient } from './EthereumClient'
 
 describe(EthereumClient.name, () => {
-  describe(EthereumClient.prototype.getLogsSinceGenesis.name, () => {
+  describe(EthereumClient.prototype.getLogsInRange.name, () => {
     it('gets the log starting from the genesis block', async () => {
       const provider = mockObject<PublicClient>({
         getLogs: mockFn().returnsOnce([]),
@@ -13,13 +13,43 @@ describe(EthereumClient.name, () => {
       const ethereumClient = new EthereumClient(provider, Logger.SILENT)
       const eventAbi = parseAbiItem('event BatchAppended(address sender)')
 
-      await ethereumClient.getLogsSinceGenesis(eventAbi, Hex('0x1234'))
+      await ethereumClient.getLogsInRange(eventAbi, EthereumAddress.ZERO, 42n)
 
       expect(provider.getLogs).toHaveBeenCalledWith({
+        address: EthereumAddress.ZERO.toString() as ViemHex,
         event: eventAbi,
-        address: '0x1234',
-        fromBlock: 0n,
+        fromBlock: 42n,
         toBlock: undefined,
+      })
+    })
+  })
+
+  describe(EthereumClient.prototype.getTransaction.name, () => {
+    it('gets the log starting from the genesis block', async () => {
+      const provider = mockObject<PublicClient>({
+        getTransaction: mockFn().returnsOnce([]),
+      })
+      const ethereumClient = new EthereumClient(provider, Logger.SILENT)
+
+      await ethereumClient.getTransaction(Hex('0x1234'))
+
+      expect(provider.getTransaction).toHaveBeenCalledWith({
+        hash: '0x1234',
+      })
+    })
+  })
+
+  describe(EthereumClient.prototype.getBlockHeader.name, () => {
+    it('gets the log starting from the genesis block', async () => {
+      const provider = mockObject<PublicClient>({
+        getBlock: mockFn().returnsOnce([]),
+      })
+      const ethereumClient = new EthereumClient(provider, Logger.SILENT)
+
+      await ethereumClient.getBlockHeader(Hex('0x1234'))
+
+      expect(provider.getBlock).toHaveBeenCalledWith({
+        blockHash: '0x1234',
       })
     })
   })
