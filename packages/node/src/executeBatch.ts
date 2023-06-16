c2d67322bfcf180fbbb7463fc030f36a2849e6d9
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-06-16 14:57:54 +0200

    Refine executeBatch and remove TODO

--- packages/node/src/executeBatch.ts
+++ packages/node/src/executeBatch.ts
@@ -33,8 +33,10 @@ const defaultState: StateMapValue = {
   nonce: Unsigned64(0),
 }
 
-// TODO(radomski): Define what is the correct way to handle any of the
-// assertions below firing.
+// NOTE(radomski): If a single transaction in the entire batch fails the entire
+// batch is dropped. This is by design to reduce complexity. In the real world
+// you would try to order transaction in such a way to maximie the fee you're
+// going to receive as well as the amount of transactions that will go through.
 export function executeBatch(
   state: StateMap,
   batch: TransactionBatch,
@@ -46,23 +48,32 @@ export function executeBatch(
     defaultState,
   )
 
-  assert(batchPoster !== Hex(0x0000000000000000000000000000000000000000))
-
   for (const tx of batch) {
     // Step 0. Check transaction type
-    assert(tx.from !== Hex(0) && tx.to !== Hex(0))
+    assert(
+      tx.from !== Hex(0) && tx.to !== Hex(0),
+      'Invalid recipient or sender',
+    )
 
     const fromAccount = getOrInsert(state, tx.from.toString(), defaultState)
     const toAccount = getOrInsert(state, tx.to.toString(), defaultState)
 
     // Step 1. Update nonce
-    assert(fromAccount.nonce === Unsigned64(Unsigned64.toBigInt(tx.nonce) - 1n))
+    assert(
+      fromAccount.nonce === Unsigned64(Unsigned64.toBigInt(tx.nonce) - 1n),
+      `Invalid nonce, expected/got = ${
+        Unsigned64.toBigInt(fromAccount.nonce) + 1n
+      }/${Unsigned64.toBigInt(tx.nonce)}`,
+    )
     fromAccount.nonce = tx.nonce
 
     // Step 2. Subtract spending
     assert(
       Unsigned64.toBigInt(fromAccount.balance) >=
         Unsigned64.toBigInt(tx.value) + Unsigned64.toBigInt(tx.fee),
+      `Balance too low, needed/got = ${
+        Unsigned64.toBigInt(tx.fee) + Unsigned64.toBigInt(tx.value)
+      }/${Unsigned64.toBigInt(fromAccount.balance)}`,
     )
 
     fromAccount.balance = Unsigned64(
