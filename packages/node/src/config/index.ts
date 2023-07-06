4873577033dc0ae9a51de8346c5ad1e14d980be0
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-07-06 13:50:54 +0200

    Make config reading type safe

--- packages/node/src/config/index.ts
+++ packages/node/src/config/index.ts
@@ -1,29 +1,30 @@
 import { EthereumAddress, Hex } from '@byor/shared'
 import fs from 'fs'
+import * as z from 'zod'
 
 import { Config } from './Config'
 
 export type { Config }
 
-interface ConfigJSON {
-  PROBE_PERIOD_SEC: number
-  FLUSH_PERIOD_SEC: number
-  CHAIN_ID: number
-  CONTRACT_CREATED_AT_BLOCK: number
-  DB_PATH: string
-  DB_MIGRATIONS_PATH: string
-  PRIVATE_KEY: string
-  CTC_CONTRACT_ADDRESS: string
-  GENESIS_FILE_PATH: string
-  RPC_SERVE_PORT: number
-  L1_TX_BATCH_GAS_LIMIT: number
-}
+const ConfigJSON = z.object({
+  PROBE_PERIOD_SEC: z.number(),
+  FLUSH_PERIOD_SEC: z.number(),
+  CHAIN_ID: z.number(),
+  CONTRACT_CREATED_AT_BLOCK: z.number(),
+  DB_PATH: z.string(),
+  DB_MIGRATIONS_PATH: z.string(),
+  PRIVATE_KEY: z.string(),
+  CTC_CONTRACT_ADDRESS: z.string(),
+  GENESIS_FILE_PATH: z.string(),
+  RPC_SERVE_PORT: z.number(),
+  L1_TX_BATCH_GAS_LIMIT: z.number(),
+})
 
 export function getConfig(configPath: string): Config {
   const jsonContent = fs.readFileSync(configPath, 'utf-8')
-  const configJson = JSON.parse(jsonContent) as ConfigJSON
+  const configJson = ConfigJSON.parse(JSON.parse(jsonContent))
 
-  const config: Config = {
+  return {
     probePeriodSec: configJson.PROBE_PERIOD_SEC,
     flushPeriodSec: configJson.FLUSH_PERIOD_SEC,
     chainId: configJson.CHAIN_ID,
@@ -36,6 +37,4 @@ export function getConfig(configPath: string): Config {
     rpcServePort: configJson.RPC_SERVE_PORT,
     gasLimit: configJson.L1_TX_BATCH_GAS_LIMIT,
   }
-
-  return config
 }
