d0076a99a23a1ba348437cb19ffba05c7f3c5525
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-07-06 13:35:43 +0200

    Refactor and document index file

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -5,6 +5,19 @@ import { getConfig } from './config'
 
 export type { RootRouter } from './api/types/AppRouter'
 
+/**
+ * Main entry point. It reads the config file and starts the node.
+ */
+async function main(configPath: string): Promise<void> {
+  const config = getConfig(configPath)
+  const app = new Application(config)
+  await app.start()
+}
+
+/**
+ * Command line interface of the program. There is a single argument, the
+ * path to the config file.
+ */
 const cmd = command({
   name: 'node',
   description: 'L2 Node for a sovereign rollup',
@@ -12,18 +25,10 @@ const cmd = command({
   args: {
     configPath: positional({ type: string, displayName: 'configPath' }),
   },
-  handler: async ({ configPath }) => {
-    await main(configPath)
-  },
+  handler: (args) => main(args.configPath),
 })
 
 run(cmd, process.argv.slice(2)).catch((e) => {
-  console.log(e)
+  console.error(e)
   process.exit(1)
 })
-
-async function main(configPath: string): Promise<void> {
-  const config = getConfig(configPath)
-  const app = new Application(config)
-  await app.start()
-}
