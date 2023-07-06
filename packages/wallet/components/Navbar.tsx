a3567be04708b398b055a3a82fc859bb20c7e42f
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-07-06 11:02:52 +0200

    Centered and added "made by"

--- packages/wallet/components/Navbar.tsx
+++ packages/wallet/components/Navbar.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link'
+import { L2BeatLogo } from './L2BeatLogo'
+
 export function Navbar(): JSX.Element {
   return (
-    <h2 className="font-semibold ml-3 text-4xl italic py-2">
-      Build Your Own Rollup
-    </h2>
+    <div className="container flex font-semibold py-2 items-end">
+      <span className="text-4xl italic">Build Your Own Rollup</span>
+      <span className="text-sm pl-2">Made By</span>
+      <Link href="https://l2beat.com">
+        <div className="pl-2">
+          <L2BeatLogo />
+        </div>
+      </Link>
+    </div>
   )
 }
