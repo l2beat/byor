d3ff73837a7bbd33b28ed6618b514944ca316e43
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-07-06 11:13:59 +0200

    Add Github

--- packages/wallet/components/Navbar.tsx
+++ packages/wallet/components/Navbar.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 import { L2BeatLogo } from './L2BeatLogo'
+import { Github } from 'lucide-react'
 
 export function Navbar(): JSX.Element {
   return (
-    <div className="container flex font-semibold py-2 items-end">
+    <div className="container flex font-semibold py-2 items-end px-0">
       <span className="text-4xl italic">Build Your Own Rollup</span>
       <span className="text-sm pl-2">Made By</span>
       <Link href="https://l2beat.com">
@@ -11,6 +12,11 @@ export function Navbar(): JSX.Element {
           <L2BeatLogo />
         </div>
       </Link>
+      <div className="flex ml-auto">
+        <Link href="https://github.com/l2beat/byor">
+        <Github />
+        </Link>
+      </div>
     </div>
   )
 }
