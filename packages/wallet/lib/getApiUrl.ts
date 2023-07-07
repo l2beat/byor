42cdc9621a4a9148f40859f636aea7948d44b2df
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-07-07 14:48:05 +0200

    Update api url

--- packages/wallet/lib/getApiUrl.ts
+++ packages/wallet/lib/getApiUrl.ts
@@ -1,6 +1,6 @@
 export function getApiUrl(): string {
   if (process.env.NODE_ENV === 'production') {
-    return 'https://byor-l2beat.fly.dev'
+    return 'https://byor-api.l2beat.com'
   }
 
   return 'http://localhost:3000'
