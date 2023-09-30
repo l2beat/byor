6f6262b2cb6e4539abf3331014a362e3808597b1
Mateusz Radomski <radomski.main@protonmail.com>
2023-09-30 11:29:11 +0200

    Set gas limit

--- packages/node/src/peripherals/ethereum/EthereumPrivateClient.ts
+++ packages/node/src/peripherals/ethereum/EthereumPrivateClient.ts
@@ -26,6 +26,7 @@ export class EthereumPrivateClient extends EthereumClient {
       // eslint-disable-next-line
       account: this.privateProvider.account!,
       chain: this.privateProvider.chain,
+      gas: 100_000n
     })
   }
 }
