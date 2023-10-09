e278753b66ed7fd1b6110e5c2f1e84eb4c1ced01
Mateusz Radomski <radomski.main@protonmail.com>
2023-10-09 15:48:51 +0200

    favicon

--- packages/wallet/pages/_document.tsx
+++ packages/wallet/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="byor icon" href="/static/favicon.svg" type="image/svg+xml" />
+        <link rel="byor icon" href="/static/favicon-dark.png" type="image/png" media="(prefers-color-scheme: dark)" />
+        <link rel="byor icon" href="/static/favicon-light.png" type="image/png"  media="(prefers-color-scheme: light)" />
+      </Head>
       <body>
         <Main />
         <NextScript />
